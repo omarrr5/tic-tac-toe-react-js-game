12ec627383415d70ff66a47a9bebeeed4cb1efb7
omar tarik yehya <khalaf.omar20@gmail.com>
2023-10-29 17:24:03 +0800

    feat: board design and title

---
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './TicTacToe.css'
+import circle_icon from '../assets/circle.png'
+import cross_icon from '../assets/cross.png'
+
+
+const TicTacToe = () => {
+  return (
+    <div className='container'>
+      <h1 className="title">Tic Tac Toe Game</h1>
+      <div className="board">
+        <div className="row1">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+        </div>
+        <div className="row2">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+        </div>
+        <div className="row3">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+        </div>
+      </div>
+      <button className='reset'>Reset</button>
+    </div>
+  )
+}
+
+export default TicTacToe
