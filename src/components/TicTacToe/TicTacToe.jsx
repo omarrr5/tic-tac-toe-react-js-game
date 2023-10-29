479490f240ec54151f167ee135f273ef8faee054
omar tarik yehya <khalaf.omar20@gmail.com>
2023-10-29 17:34:03 +0800

    feat: built toggle logic

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './TicTacToe.css'
 import circle_icon from '../assets/circle.png'
 import cross_icon from '../assets/cross.png'
 
+let data = ["","","","","","","","",""];
 
 const TicTacToe = () => {
+
+    let[count,setCount] = useState(0);
+    let[lock,setLock] = useState(false);
+
+    const toggle = (e,num) =>{
+        if(lock){
+            return 0;
+        }
+
+        if(count%2===0){
+            e.target.innerHTML = `<img src= '${cross_icon}'>`;
+            data[num] = x;
+            setCount(++count);
+        }
+        else{
+            e.target.innerHTML = `<img src= '${circle_icon}'>`;
+            data[num] = 0;
+            setCount(++count);
+        }
+    }
+
   return (
     <div className='container'>
       <h1 className="title">Tic Tac Toe Game</h1>
