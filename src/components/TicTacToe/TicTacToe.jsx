8210a2e4ad104bf9e166868d6897b458af00712e
omar tarik yehya <khalaf.omar20@gmail.com>
2023-10-29 17:40:19 +0800

    feat: improved toggle functionality

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -17,12 +17,12 @@ const TicTacToe = () => {
 
         if(count%2===0){
             e.target.innerHTML = `<img src= '${cross_icon}'>`;
-            data[num] = x;
+            data[num] = "x";
             setCount(++count);
         }
         else{
             e.target.innerHTML = `<img src= '${circle_icon}'>`;
-            data[num] = 0;
+            data[num] = "o";
             setCount(++count);
         }
     }
@@ -32,19 +32,19 @@ const TicTacToe = () => {
       <h1 className="title">Tic Tac Toe Game</h1>
       <div className="board">
         <div className="row1">
-            <div className="boxes"></div>
-            <div className="boxes"></div>
-            <div className="boxes"></div>
+            <div className="boxes" onClick={(e)=>{toggle(e,0)}}></div>
+            <div className="boxes" onClick={(e)=>{toggle(e,1)}}></div>
+            <div className="boxes" onClick={(e)=>{toggle(e,2)}}></div>
         </div>
         <div className="row2">
-            <div className="boxes"></div>
-            <div className="boxes"></div>
-            <div className="boxes"></div>
+            <div className="boxes" onClick={(e)=>{toggle(e,3)}}></div>
+            <div className="boxes" onClick={(e)=>{toggle(e,4)}}></div>
+            <div className="boxes" onClick={(e)=>{toggle(e,5)}}></div>
         </div>
         <div className="row3">
-            <div className="boxes"></div>
-            <div className="boxes"></div>
-            <div className="boxes"></div>
+            <div className="boxes" onClick={(e)=>{toggle(e,6)}}></div>
+            <div className="boxes" onClick={(e)=>{toggle(e,7)}}></div>
+            <div className="boxes" onClick={(e)=>{toggle(e,8)}}></div>
         </div>
       </div>
       <button className='reset'>Reset</button>
